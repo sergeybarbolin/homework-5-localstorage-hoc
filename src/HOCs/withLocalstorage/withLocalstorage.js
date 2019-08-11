c98bdb38cfd1a868f304112a161f499f73f0bbbf
sergeybarbolin <sergeybarbolin25@gmail.com>
2019-08-11 23:35:05 +0300

    added todo

--- src/HOCs/withLocalstorage/withLocalstorage.js
+++ src/HOCs/withLocalstorage/withLocalstorage.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 import { load, save } from '../../localstorage';
 
-const withLocalstorage = () => () => {};
+const withLocalstorage = (key, initValue) => WrappedComponent => {
+    class LocalStorage extends Component {
+        loadData = () => load(key) || initValue
+
+        saveData = data => {
+            save(key, data);
+            this.forceUpdate();
+        }
+
+        render() {
+            const {forwardedRef, ...rest} = this.props
+            
+            return <WrappedComponent ref={forwardedRef} {...rest} savedData={this.loadData()} saveData={this.saveData} />
+        }
+    }
+
+    const forwardRef = (props, ref) => {
+        return <LocalStorage {...props} ref={ref} /> 
+    }
+    
+    return React.forwardRef(forwardRef);
+};
 
 export default withLocalstorage;
